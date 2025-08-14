6084060b7d2108c2c241d4ece1b74db88298a549
Niels Jacobs <jacobsniels10@gmail.com>
2025-08-14 20:15:46 +0200

    working docker

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,15 +1,17 @@
 import '@testing-library/jest-dom';
-import { TextEncoder, TextDecoder } from 'util';
 
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+if (typeof global !== 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
 
-Object.defineProperty(globalThis, 'import', {
-  value: {
-    meta: {
-      env: {
-        VITE_API_URL: 'http://localhost:3001/api'
+  Object.defineProperty(globalThis, 'import', {
+    value: {
+      meta: {
+        env: {
+          VITE_API_URL: 'http://localhost:3001/api'
+        }
       }
     }
-  }
-});
+  });
+}
